f42b411a0496d4c9eaa8d9d82eaf36ec3477e30a
fullStackZac <zac.childress.sr@gmail.com>
2022-03-23 16:46:25 -0500

    Chapter 3 - Variables & Assignment Solutions

---
+++ learn_to_program_chris_pine/ch_03_variables_and_assignment/variables.rb
@@ -0,0 +1,23 @@
+my_string = "...You can say that again..."
+name = "Anya Christina Emanuella Jenkins Harris"
+
+puts my_string
+puts my_string
+
+puts "My name is " + name
+puts "Wow! " + name "\nis a really long name!"
+
+puts "My name is #{name}"
+puts "Wow! #{name}\nis a really long name!"
+
+composer = "Mozart"
+puts "#{composer} was all the rage in his day."
+
+composer = "Beethoven"
+puts "But I prefer #{composer} myself."
+
+my_own_var = "Just another " + "string"
+puts my_own_var
+
+my_own_var = 5 * (1 + 2)
+puts my_own_var
