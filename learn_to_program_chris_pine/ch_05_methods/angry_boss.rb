b0aac6c2b7dbeef1dab1544c29a76bc7282c6d5f
fullStackZac <zac.childress.sr@gmail.com>
2022-03-23 17:46:35 -0500

    Chapter 5 - Methods Exercise Solutions

---
+++ learn_to_program_chris_pine/ch_05_methods/angry_boss.rb
@@ -0,0 +1,4 @@
+puts "WHADAYA WANT?"
+response = gets.chomp
+
+puts "WADAYA MEAN \"#{response.upcase}\" ? YOU\'RE FIRED!"
---
+++ learn_to_program_chris_pine/ch_05_methods/methods.rb
@@ -0,0 +1,15 @@
+var1 = "stop"
+var2 = "deliver repaid desserts"
+var3 = "....TCELES B HSUP - a magic spell?"
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+puts "What is your full name?"
+name = gets.chomp
+
+puts "Did you know there are " + name.length + " characters in your full name?"
